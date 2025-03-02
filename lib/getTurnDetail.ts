086df8042ac4e77b56649d0335e492036aa5b6fd
Lucas <lucas-mateus.dc@hotmail.com>
2025-03-02 19:39:39 -0300

    feat(fetch): created fetch to get list of turns and turn data

---
+++ lib/getTurnDetail.ts
@@ -0,0 +1,15 @@
+import { TurnData } from "@/app/types/turn";
+
+export async function getTurnDetailById(slug: string): Promise<TurnData> {
+  // await new Promise((resolve) => setTimeout(resolve, 2500));
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_APP_BASE_URL}/api/turn/${slug}`,
+    {
+      cache: "no-store",
+    }
+  );
+
+  if (!response.ok) throw new Error("Erro ao buscar os turnos disponíveis");
+
+  return response.json();
+}
---
+++ lib/getTurns.ts
@@ -0,0 +1,15 @@
+import { TurnCard } from "@/app/types/turn";
+
+export async function getTurns(): Promise<TurnCard[]> {
+  await new Promise((resolve) => setTimeout(resolve, 2500));
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_APP_BASE_URL}/api/turn`,
+    {
+      cache: "no-store",
+    }
+  );
+
+  if (!response.ok) throw new Error("Erro ao buscar os turnos disponíveis");
+
+  return response.json();
+}
