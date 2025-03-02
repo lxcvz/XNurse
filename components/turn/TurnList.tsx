1e237b55ce3aa491ab836414ae6d46fd6c0fbf4b
Lucas <lucas-mateus.dc@hotmail.com>
2025-03-02 20:59:09 -0300

    feat(list): added accordion to show filter on responsive list design

--- components/turn/TurnList.tsx
+++ components/turn/TurnList.tsx
@@ -1,90 +1,35 @@
 "use client";
 import { TurnCard } from "@/app/types/turn";
 import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
 import { useRouter } from "next/navigation";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "../ui/accordion";
+import { Filter } from "@/components/Filter";
+import { useMediaQuery } from "react-responsive";
 
 export function TurnList({ data }: { data: TurnCard[] }) {
   const router = useRouter();
+  const isMobile = useMediaQuery({ maxWidth: 768 });
 
   return (
     <main className="container mx-auto py-6 px-4 md:px-0">
       <div className="flex flex-col md:flex-row gap-6">
-        <div className="w-full md:w-1/4 border p-4 rounded-lg h-max">
-          <h2 className="text-xl font-semibold text-gray-800 mb-4">
-            Filtros // incoming
-          </h2>
-
-          <div className="mb-6">
-            <h3 className="font-medium text-gray-700 mb-2">Período</h3>
-            <div className="space-y-2">
-              <div className="flex items-center space-x-2">
-                <Checkbox id="terms" />
-                <label
-                  htmlFor="terms"
-                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                >
-                  Manhã
-                </label>
-              </div>
-
-              <div className="flex items-center space-x-2">
-                <Checkbox id="terms" />
-                <label
-                  htmlFor="terms"
-                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                >
-                  Tarde
-                </label>
-              </div>
-
-              <div className="flex items-center space-x-2">
-                <Checkbox id="terms" />
-                <label
-                  htmlFor="terms"
-                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                >
-                  Noite
-                </label>
-              </div>
-            </div>
-          </div>
-
-          <div className="mb-6">
-            <h3 className="font-medium text-gray-700 mb-2">Carga horária</h3>
-            <div className="space-y-2">
-              <div className="flex items-center space-x-2">
-                <Checkbox id="terms" />
-                <label
-                  htmlFor="terms"
-                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                >
-                  Até 30h semanais
-                </label>
-              </div>
-              <div className="flex items-center space-x-2">
-                <Checkbox id="terms" />
-                <label
-                  htmlFor="terms"
-                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                >
-                  40h semanais
-                </label>
-              </div>
-              <div className="flex items-center space-x-2">
-                <Checkbox id="terms" />
-                <label
-                  htmlFor="terms"
-                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                >
-                  44h semanais
-                </label>
-              </div>
-            </div>
-          </div>
-
-          <Button className="w-full cursor-pointer">Aplicar Filtros</Button>
-        </div>
+        {isMobile ? (
+          <Accordion type="single" collapsible className="w-full">
+            <AccordionItem value="item-1">
+              <AccordionTrigger>Filtros</AccordionTrigger>
+              <AccordionContent>
+                <Filter />
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        ) : (
+          <Filter />
+        )}
 
         <div className="w-full md:w-3/4">
           <h2 className="text-xl font-semibold text-gray-800 mb-4">
