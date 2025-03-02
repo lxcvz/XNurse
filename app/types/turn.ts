e79ca40cb2da9482443d10aedf2bab4987b41147
Lucas <lucas-mateus.dc@hotmail.com>
2025-03-02 19:40:27 -0300

    chore(type): created types to handle data

---
+++ app/types/turn.ts
@@ -0,0 +1,25 @@
+export type TurnData = {
+  id: number;
+  clientName: string;
+  website: string;
+  businessType: string;
+  location: string;
+  service: string;
+  serviceLocation: string;
+  value: string;
+  hours: string;
+  startDate: string;
+  endDate: string;
+  period: string;
+  mission: string;
+  description: string;
+};
+
+export type TurnCard = {
+  id: number;
+  title: string;
+  location: string;
+  salary: string;
+  hours: string;
+  period: string;
+};
